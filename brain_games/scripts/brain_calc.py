ef87ce535c4a8f40be6d8109b2d8beb146ffbe01
Vladislav Akashev <vlad.akashev.w@gmail.com>
2023-03-30 17:19:41 +0300

    Add game 'Brain GCD'

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -21,23 +21,13 @@ def subtract(num1, num2):
 operations = [(custom_sum, '+'), (subtract, '-'), (multiply, '*')]
 
 
-def is_even(num):
-    return num % 2 == 0
-
-
 def get_round_data():
     num1 = random.randint(0, 10)
     num2 = random.randint(0, 10)
     operation_index = random.randint(0, 2)
     operation, symbol = operations[operation_index]
-    print(f"symbol {symbol}")
     question = f'Question: {num1} {symbol} {num2}'
-
-    print(f"operation {operation}")
-
     correct_answer = str(operation(num1, num2))
-    print(f"correct_answer {correct_answer}")
-
     return question, correct_answer
 
 

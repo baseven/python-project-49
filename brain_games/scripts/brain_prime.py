c355a7ea569947fad77eb9f1de35ea4024c7b08d
Vladislav Akashev <vlad.akashev.w@gmail.com>
2023-03-30 21:35:02 +0300

    Add game 'Brain Prime'

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,28 @@
+import random
+
+from brain_games.game_engine import engine
+
+
+game_task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(num):
+    for i in range(2, int(num / 2)):
+        if (num % i) == 0:
+            return False
+    return True
+
+
+def get_round_data():
+    num = random.randint(1, 50)
+    question = f'Question: {num}'
+    correct_answer = 'yes' if is_prime(num) else 'no'
+    return question, correct_answer
+
+
+def main():
+    engine(game_task, get_round_data)
+
+
+if __name__ == '__main__':
+    main()
